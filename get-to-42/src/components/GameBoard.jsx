eccffdecbac8979d97af20b8cd69f82712cecf46
ormiller-hilma <ormiller@hilma.tech>
2025-11-02 16:51:41 +0200

    nothing basicaly

--- get-to-42/src/components/GameBoard.jsx
+++ get-to-42/src/components/GameBoard.jsx
@@ -4,15 +4,14 @@ import LoginPage from "./LoginPage";
 
 function GameBoard({ username, isActivePlayer, passTurn }) {
   const [number, setNumber] = useState(Math.floor(Math.random() * 42));
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   const isWinning = number === 42;
-  // const isLoggedIn = false; // temp value
 
   return (
     <>
       {isLoggedIn && (
-        <div style={{ opacity: 0.5 + isActivePlayer * 0.5 }}>
+        <div style={{ opacity: 0.35 + isActivePlayer * 0.65 }}>
           <h2>{username}</h2>
           <h1>{number}</h1>
           <ActionButtons
