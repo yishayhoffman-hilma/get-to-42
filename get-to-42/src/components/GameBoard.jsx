282ee7f0bb6cf81ff5b88832549b1f40d5ec758c
yishiehoffman-hilma <yishiehoffman@hilma.tech>
2025-11-03 12:32:07 +0200

    made leaderboard build

--- get-to-42/src/components/GameBoard.jsx
+++ get-to-42/src/components/GameBoard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import ActionButtons from "./ActionButtons";
 import LoginPage from "./LoginPage";
 import RegisterPage from "./RegisterPage";
